f695e7f41ad18be833356c093bc30f4f95b32286
Robert Wolff <robert@thedeveco.com>
2025-02-02 17:24:37 -0800

    Added new .js files for capture bot data
    Signed-off-by: Robert Wolff <robert@thedeveco.com>

--- assets/js/botstats.js
+++ assets/js/botstats.js
@@ -1,39 +1,50 @@
 async function fetchBotStats() {
-    try {
-      const response = await fetch("https://api.portalnet.work/stats");
-      const data = await response.json();
-      document.getElementById("totalServers").textContent =
-        data.currentGuildCount.toLocaleString();
-      document.getElementById("comptotalServers").textContent = formatServerCount(
-        data.currentGuildCount
-      );
-      document.getElementById("totalUsers").textContent =
-        data.totalUserCount.toLocaleString();
-      document.getElementById("comptotalUsers").textContent = formatUserCount(
-        data.totalUserCount
-      );
-      document.getElementById("totalcommandsCount").textContent =
-        data.commandsCount.toLocaleString();
-      document.getElementById("totalcommandUsage").textContent =
-        data.totalUsage.toLocaleString();
-    } catch (error) {
-      console.error("Failed to fetch bot stats:", error);
-    }
+  try {
+    const response = await fetch("https://api.portalnet.work/stats");
+    const data = await response.json();
+    document.getElementById("totalServers").textContent =
+      data.currentGuildCount.toLocaleString();
+    document.getElementById("comptotalServers").textContent = formatServerCount(
+      data.currentGuildCount
+    );
+    document.getElementById("totalUsers").textContent =
+      data.totalUserCount.toLocaleString();
+    document.getElementById("comptotalUsers").textContent = formatUserCount(
+      data.totalUserCount
+    );
+    document.getElementById("totalNetwork").textContent =
+      data.networkServersCount.toLocaleString();
+    //document.getElementById("comptotalNetwork").textContent = formatServerCount(
+    //  data.networkServersCount
+    // );
+    document.getElementById("totalMessages").textContent =
+      data.messagecount.toLocaleString();
+    document.getElementById("comptotalMessages").textContent =
+      formatMessageCount(data.messagecount);
+  } catch (error) {
+    console.error("Failed to fetch bot stats:", error);
   }
-  
-  function formatServerCount(count) {
-    if (count >= 100) {
-      return Math.floor(count / 10) * 10 + "+";
-    }
-    return count.toLocaleString();
+}
+
+function formatServerCount(count) {
+  if (count >= 100) {
+    return Math.floor(count / 10) * 10 + "+";
   }
-  
-  function formatUserCount(count) {
-    if (count >= 1000) {
-      return (count / 1000).toFixed(1) + "k+";
-    }
-    return count.toLocaleString();
+  return count.toLocaleString();
+}
+
+function formatUserCount(count) {
+  if (count >= 1000) {
+    return (count / 1000).toFixed(1) + "k+";
   }
-  
-  window.onload = fetchBotStats;
-  
+  return count.toLocaleString();
+}
+
+function formatMessageCount(count) {
+  if (count >= 1000) {
+    return (count / 1000).toFixed(1) + "k+";
+  }
+  return count.toLocaleString();
+}
+
+window.onload = fetchBotStats;
