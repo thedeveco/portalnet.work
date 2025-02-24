f53edb3642672dc11371c002b6b0310b5d06e938
Robert Wolff <robert@thedeveco.com>
2025-02-23 22:11:16 -0800

    Attempt to fix Support linkage
    Signed-off-by: Robert Wolff <robert@thedeveco.com>

--- assets/js/server.mjs
+++ assets/js/server.mjs
@@ -29,7 +29,6 @@ const htmlRoutes = [
   { route: "/tos", file: "../../tos.html" },
   { route: "/listing", file: "../../legal.html" },,
   { route: "/invite", file: "../../invite.html" },
-  { route: "/support", file: "../../support.html" },
 ];
 
 htmlRoutes.forEach((route) => serveHTML(route.route, route.file));
